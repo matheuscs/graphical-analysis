533aa204f5ec137a0fd05db7e7fa53e2f8134ce4
Matheus Silva <matheuscarvas@gmail.com>
2017-11-05 21:38:26 -0200

    segregating ifr analysis and plot functions

--- graphical_analysis/indicators_analysis/ifr_analysis.py
+++ graphical_analysis/indicators_analysis/ifr_analysis.py
@@ -5,8 +5,18 @@ from graphical_analysis.indicators.ifr import find_ifr
 
 
 def analyse_ifr(stocks_data):
+    # tbd
+    return find_ifr(stocks_data)
+
+
+def plot_ifr(stocks_data, stock):
+    closes = stocks_data[stock][['Close']]
+    plt.subplot(211)
+    plt.plot(closes)
+
     labels, ifr = find_ifr(stocks_data)
-    # print(stocks_data)
+    plt.subplot(212)
     plt.plot(ifr)
     # plt.xlabel(labels)
+
     plt.show()
--- graphical_analysis/main.py
+++ graphical_analysis/main.py
@@ -1,6 +1,6 @@
 
 from graphical_analysis.candlesticks_analysis.gap_analysis import analyse_gap
-from graphical_analysis.indicators_analysis.ifr_analysis import analyse_ifr
+from graphical_analysis.indicators_analysis.ifr_analysis import analyse_ifr, plot_ifr
 
 from helpers.const import Interval
 from helpers.stocks import get_stocks_symbols, get_stocks_data
@@ -8,7 +8,8 @@ from helpers.stocks import get_stocks_symbols, get_stocks_data
 intv = Interval()
 
 stocks_data = get_stocks_data(get_stocks_symbols(), intv.DAY)
-analyse_ifr(stocks_data)
+print(analyse_ifr(stocks_data))
+plot_ifr(stocks_data, 'BBAS3')
 
 
 
