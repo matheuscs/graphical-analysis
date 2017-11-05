8efe87cf064af6385e26e70c97d0fe8c9881eabc
Matheus Silva <matheuscarvas@gmail.com>
2017-11-05 20:20:30 -0200

    adding ifr indicator functionality

--- graphical_analysis/main.py
+++ graphical_analysis/main.py
@@ -1,9 +1,14 @@
 
 from graphical_analysis.candlesticks_analysis.gap_analysis import analyse
+from graphical_analysis.indicators.ifr import find_ifr
 from helpers.const import Interval
 from helpers.stocks import get_stocks_symbols, get_stocks_data
 
 intv = Interval()
 
-a = analyse(get_stocks_data(get_stocks_symbols(), intv.DAY))
+stocks_data = get_stocks_data(get_stocks_symbols(), intv.DAY)
+
+# a = analyse(stocks_data)
 # print(a)
+
+print(find_ifr(stocks_data))
