152054dbf116860662b5726607f974ae4de5dcb6
Matheus Silva <matheuscarvas@gmail.com>
2017-11-13 00:08:57 -0200

    hanging man analysis kick off

---
+++ graphical_analysis/candlesticks/hanging_man.py
@@ -0,0 +1,38 @@
+from graphics.patterns.bodies import find_long_lower_shadows
+from graphics.patterns.bodies import is_body_higher_than
+from helpers.dataframe import previous_row, next_row
+
+
+def analyse_hanging_mans(stocks_data):
+    """
+    Hammer definition:
+        - Long lower shadow
+        - Upward movement
+
+    Higher significance factors:
+        - Downward trend
+        - Under a resistance area
+        - Higher volume than previous candlestick
+
+    Next candlestick:
+        - Negative
+        - Opening lower than hanging man body
+
+    Conclusion:
+        - Reversal of movement
+        - Sell signal
+    """
+
+    result = {}
+    for k, df in stocks_data.items():
+        df_hm = find_long_lower_shadows(df)
+        for i, r in df_hm.iterrows():
+            r0 = previous_row(df, i)
+            r1 = df.ix[i]
+            r2 = next_row(df, i)
+            # upward movement and next cs lower
+            if is_body_higher_than(r1, r0) and is_body_higher_than(r1, r2):
+                result[k] = i
+
+    print(result)
+
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 
-from graphical_analysis.indicators.rsi import plot_rsi
+from graphical_analysis.candlesticks.shooting_star import analyse_shooting_stars
+from graphical_analysis.candlesticks.hammer import analyse_hammers
+from graphical_analysis.candlesticks.hanging_man import analyse_hanging_mans
 
 from helpers.const import Interval
 from helpers.stocks import get_stocks_symbols, get_stocks_data
 
 intv = Interval()
 
-stocks_data = get_stocks_data(get_stocks_symbols(), intv.DAY, "6M")
-plot_rsi(stocks_data, 'MGLU3')
-
+stocks_data = get_stocks_data(get_stocks_symbols(), intv.DAY, "2M")
+# analyse_hammers(stocks_data)
+# analyse_shooting_stars(stocks_data)
+analyse_hanging_mans(stocks_data)
 
