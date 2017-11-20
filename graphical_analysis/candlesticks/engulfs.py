bd351dc8020109b45b4c94b2890ba20d0bcd53ac
Matheus Silva <matheuscarvas@gmail.com>
2017-11-20 20:22:17 -0200

    bulls and bears togethers on the engulf

---
+++ graphical_analysis/candlesticks/engulfs.py
@@ -0,0 +1,45 @@
+from graphics.patterns.candlesticks import candlestick_positioning
+from graphics.patterns.candlesticks import is_candlestick_positive
+
+
+def analyse_engulfs(stocks_data):
+
+    for stock_symbol, df in stocks_data.items():
+        bullishs_engulf = []
+        for i in range(1, len(df)):
+            engulfeds = 0
+            current_row = df.ix[i]
+            for j in range(1, i):
+                previous_row = df.ix[i - j]
+                if is_candlestick_positive(previous_row) or \
+                        not is_candlestick_positive(current_row) or \
+                        candlestick_positioning(previous_row, current_row) != 1:
+                    break
+                engulfeds += 1
+            if engulfeds > 0:
+                bullishs_engulf.append((df.index[i], engulfeds))
+
+        bearishs_engulf = []
+        for i in range(1, len(df)):
+            engulfeds = 0
+            current_row = df.ix[i]
+            for j in range(1, i):
+                previous_row = df.ix[i - j]
+                if not is_candlestick_positive(previous_row) or \
+                        is_candlestick_positive(current_row) or \
+                        candlestick_positioning(previous_row, current_row) != 1:
+                    break
+                engulfeds += 1
+            if engulfeds > 0:
+                bearishs_engulf.append((df.index[i], engulfeds))
+
+        if len(bullishs_engulf) > 0:
+            print('\n--- BULLISH ENGULFS ---')
+            print(stock_symbol)
+            [print('Date: {}, Engulfeds: {}'.format(e[0], e[1])) for e in bullishs_engulf]
+
+        if len(bullishs_engulf) > 0:
+            print('\n--- BEARISH ENGULFS ---')
+            print(stock_symbol)
+            [print('Date: {}, Engulfeds: {}'.format(e[0], e[1])) for e in bearishs_engulf]
+
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from graphical_analysis.candlesticks.bullish_engulfing import \
-    analyse_bullish_engulfing
+from graphical_analysis.candlesticks.engulfs import analyse_engulfs
 from helpers.const import Interval
 from helpers.stocks import get_stocks_symbols, get_stocks_data
 
 intv = Interval()
 
 stocks_data = get_stocks_data(get_stocks_symbols(), intv.DAY, "2M")
-analyse_bullish_engulfing(stocks_data)
+analyse_engulfs(stocks_data)
