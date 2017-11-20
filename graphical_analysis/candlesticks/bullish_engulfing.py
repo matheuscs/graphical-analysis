cb205219f09d37b95908ffdea85b89be45d4e1fa
Matheus Silva <matheuscarvas@gmail.com>
2017-11-19 23:31:52 -0200

    bullish engulfing up and running

--- graphical_analysis/candlesticks/bullish_engulfing.py
+++ graphical_analysis/candlesticks/bullish_engulfing.py
@@ -1,3 +1,6 @@
+from graphics.patterns.candlesticks import candlestick_positioning
+from graphics.patterns.candlesticks import is_candlestick_positive
+
 
 def analyse_bullish_engulfing(stocks_data):
     """
@@ -17,4 +20,26 @@ def analyse_bullish_engulfing(stocks_data):
         - Buy signal
     """
 
-    pass
+    for stock_symbol, df in stocks_data.items():
+        bullish_engulfs = []
+        for i in range(1, len(df)):
+            engulfeds = 0
+            current_row = df.ix[i]
+            for j in range(1, i):
+                previous_row = df.ix[i - j]
+                if not is_candlestick_positive(previous_row) and is_candlestick_positive(current_row):
+                    pos = candlestick_positioning(previous_row, current_row)
+                    if pos == 1:
+                        engulfeds += 1
+                    else:
+                        break
+                else:
+                    break
+            if engulfeds > 0:
+                bullish_engulfs.append((df.index[i], engulfeds))
+
+        if len(bullish_engulfs) > 0:
+            print('\n--- BULLISH ENGULFS ---')
+            print(stock_symbol)
+            [print('Date: {}, Engulfeds: {}'.format(e[0], e[1])) for e in bullish_engulfs]
+
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from graphical_analysis.candlesticks.long_shadows import analyse_long_shadows
-
+from graphical_analysis.candlesticks.bullish_engulfing import \
+    analyse_bullish_engulfing
 from helpers.const import Interval
 from helpers.stocks import get_stocks_symbols, get_stocks_data
 
 intv = Interval()
 
 stocks_data = get_stocks_data(get_stocks_symbols(), intv.DAY, "2M")
-analyse_long_shadows(stocks_data)
+analyse_bullish_engulfing(stocks_data)
