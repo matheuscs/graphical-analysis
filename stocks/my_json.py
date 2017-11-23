9ce78e792b5a56a16188d965083f986c14399531
Matheus Silva <matheuscarvas@gmail.com>
2017-11-23 20:34:54 -0200

    lots of stocks to play with now

--- stocks/my_json.py
+++ stocks/my_json.py
@@ -1,11 +1,12 @@
 import json
 
 
-def get_stocks_symbols():
+def get_stocks_symbols(stock_list='my'):
     """
     Retrieve all stocks symbols from the json
 
+    :type stock_list: object
     :return: all stocks symbols
     """
     with open(r'./data/stocks.json') as data_file:
-        return json.load(data_file)['symbols']
+        return json.load(data_file)[stock_list]
