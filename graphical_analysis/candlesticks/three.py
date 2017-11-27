eb93632bb3ddfc6f530bd9564cc9059133feeb21
Matheus Silva <matheuscarvas@gmail.com>
2017-11-26 22:10:14 -0200

    introducing evening stars

--- graphical_analysis/candlesticks/three.py
+++ graphical_analysis/candlesticks/three.py
@@ -22,8 +22,28 @@ def analyse_threes(stocks_data):
                     diff_val1 > 2 and diff_val2 > 4:
                 morning_stars.append((df.index[i], diff_pos1-diff_pos3))
 
+        evening_stars = []
+        for i in range(2, len(df)):
+            current_row = df.ix[i]
+            previous_row = df.ix[i - 1]
+            preprevious_row = df.ix[i - 2]
+            diff_pos1 = candlestick_positioning(previous_row, current_row)
+            diff_pos2 = candlestick_positioning(preprevious_row, current_row)
+            diff_pos3 = candlestick_positioning(preprevious_row, previous_row)
+            diff_val1 = candlestick_body_diff(previous_row, current_row)
+            diff_val2 = candlestick_body_diff(previous_row, preprevious_row)
+            if not is_candlestick_positive(current_row) and \
+                    is_candlestick_positive(preprevious_row) and \
+                    diff_pos1 <= -2 and diff_pos2 == 2 and diff_pos3 >= 3 and \
+                    diff_val1 > 2 and diff_val2 > 4:
+                evening_stars.append((df.index[i], diff_pos3-diff_pos1))
+
         if len(morning_stars) > 0:
             print('\n--- MORNING STARS ---')
             print(stock_symbol)
             [print('Date: {}, Significance: {}'.format(m[0], m[1])) for m in morning_stars]
 
+        if len(evening_stars) > 0:
+            print('\n--- EVENING STARS ---')
+            print(stock_symbol)
+            [print('Date: {}, Significance: {}'.format(e[0], e[1])) for e in evening_stars]
