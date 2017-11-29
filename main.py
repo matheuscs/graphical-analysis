559cf1f1d37e4b40a11c7dff231bea80d3ccf432
Matheus Silva <matheuscarvas@gmail.com>
2017-11-28 22:38:54 -0200

    the way i'm using to analyse in the main

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
+from graphical_analysis.candlesticks.engulfs import analyse_engulfs
+from graphical_analysis.candlesticks.gap import analyse_gaps
 from graphical_analysis.candlesticks.long_shadows import analyse_long_shadows
 from graphical_analysis.candlesticks.three import analyse_threes
-from stocks.my import read_stocks_data, update_db_from_request, \
-    request_stocks_data
+from stocks.my import read_stocks_data, update_db_from_request
 from stocks.my_json import get_stocks_symbols
 
 
-def update_db(period='2Y'):
+def update_db(period='1Y'):
     # update_db_from_request(period, get_stocks_symbols('ibov'), True)
     update_db_from_request(period, get_stocks_symbols('ibov'))
-    update_db_from_request(period, get_stocks_symbols('small'))
+    update_db_from_request(period, get_stocks_symbols('smll'))
     update_db_from_request(period, get_stocks_symbols('others'))
 
 
 if __name__ == '__main__':
     pass
-    analyse_threes(read_stocks_data(60, get_stocks_symbols('ibov')))
-
+    sd = read_stocks_data(20, get_stocks_symbols('ibov'))
+    analyse_threes(sd)
+    analyse_long_shadows(sd)
+    analyse_engulfs(sd)
+    analyse_gaps(sd)
+    # update_db()
