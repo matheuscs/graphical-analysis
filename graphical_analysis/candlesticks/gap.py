854c8c3bce3bc1a129f4a4c38c47d29cd4b4759c
Matheus Silva <matheuscarvas@gmail.com>
2017-11-28 22:38:21 -0200

    improving gap analysis

--- graphical_analysis/candlesticks/gap.py
+++ graphical_analysis/candlesticks/gap.py
@@ -3,8 +3,8 @@ from graphics.patterns.candlesticks import candlestick_positioning, \
 
 
 def analyse_gaps(stocks_data):
-    gaps = []
     for stock_symbol, df in stocks_data.items():
+        gaps = []
         previous_row = df.ix[0]
         for index in range(1, len(df)):
             current_row = df.ix[index]
@@ -13,6 +13,7 @@ def analyse_gaps(stocks_data):
                 gaps.append((df.index[index], diff))
             previous_row = current_row
 
-        print('\n---GAP---')
-        print(stock_symbol)
-        [print('Date: {}, Difference: {:.2f}'.format(g[0], g[1])) for g in gaps]
+        if len(gaps) > 0:
+            print('\n---GAP---')
+            print(stock_symbol)
+            [print('Date: {}, Difference: {:.2f}'.format(g[0], g[1])) for g in gaps]
